4792c5f27bd363292faf73b67c88b68ac7bd175d
Caleb Sylvest <caleb.sylvest@gmail.com>
2017-12-09 10:17:46 -0600

    build gatsby tutorial 1

---
+++ tutorial-part-one/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () =>
+  <div style={{ color: 'tomato'}}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="http://lorempixel.com/400/200/" alt="" />
+    <br/>
+    <div>
+      <Link to="/page-2/">Link</Link>
+    </div>
+    <div>
+      <Link to="/counter/">Counter</Link>
+    </div>
+  </div>
